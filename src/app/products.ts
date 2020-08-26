20f973edf21a2048387d92e52cc2730a273f0aa3
parapai <ahmadrivai94@gmail.com>
2020-08-26 16:13:22 +0700

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Kebab Special',
+    price: 799,
+    description: 'A large kebab with one of the best meats'
+  },
+  {
+    name: 'Kebab Mini',
+    price: 699,
+    description: 'A great kebab with one of the best meats'
+  },
+  {
+    name: 'Kebab Standard',
+    price: 299,
+    description: 'Just ordinary kebab'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
